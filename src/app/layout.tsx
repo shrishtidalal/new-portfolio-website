789c181eff4bcda8d4aec04c8f3269ed78bb8fac
Shrishti <shrishti.dalal@berkeley.edu>
2025-11-30 19:46:02 -0800

    favicon update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,16 +14,23 @@ import {
   SpacingToken,
 } from "@once-ui-system/core";
 import { Footer, Header, RouteGuard, Providers } from "@/components";
-import { baseURL, effects, fonts, style, dataStyle, home } from "@/resources";
+import { baseURL, effects, fonts, style, dataStyle, home, person } from "@/resources";
 
 export async function generateMetadata() {
-  return Meta.generate({
-    title: home.title,
-    description: home.description,
-    baseURL: baseURL,
-    path: home.path,
-    image: home.image,
-  });
+  return {
+    ...Meta.generate({
+      title: home.title,
+      description: home.description,
+      baseURL: baseURL,
+      path: home.path,
+      image: home.image,
+    }),
+    icons: {
+      icon: person.avatar,
+      shortcut: person.avatar,
+      apple: person.avatar,
+    },
+  };
 }
 
 export default async function RootLayout({
